bf444dd9986488d5a54a279492eec7e214e79e4c
Ebo Lee <yiboli.coding@gmail.com>
2020-07-23 04:01:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+class School
+    
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end 
+  
+  def roster
+    @roster
+  end 
+  
+  
+# is able to add a student
+# is able to add multiple students to a class (grade)
+  
+  def add_student(name,grade)
+    @school = name
+    @grade = grade
+      if !@roster.include?(grade)
+      @roster[grade] = []
+      end 
+    @roster[grade] << name
+  end 
+  
+# is able to retreive students from a grade
+ 
+  def grade(grade)
+    @roster[grade]
+  end 
+
+# is able to sort the students
+
+def sort
+    sorted_name = @roster[sorted_name.sort_by { |grade, names| names } ]
+end 
+
+end 
+
